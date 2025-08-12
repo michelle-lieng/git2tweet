c1819f01e058b1718cb64fa35f903303309444d1
Michelle <michellelieng2020@gmail.com>
2025-08-12 21:50:49 +1000

    reformat to a class

--- post_to_twitter.py
+++ post_to_twitter.py
@@ -4,14 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client = tweepy.Client(
-    consumer_key=os.getenv("TW_CONSUMER_KEY"),
-    consumer_secret=os.getenv("TW_CONSUMER_SECRET"),
-    access_token=os.getenv("TW_ACCESS_TOKEN"),            # user token (write-enabled)
-    access_token_secret=os.getenv("TW_ACCESS_TOKEN_SECRET")
-)
+class TwitterClient:
+    def __init__(self):
+        self.client = tweepy.Client(
+            consumer_key=os.getenv("TW_CONSUMER_KEY"),
+            consumer_secret=os.getenv("TW_CONSUMER_SECRET"),
+            access_token=os.getenv("TW_ACCESS_TOKEN"), #ensure write access!
+            access_token_secret=os.getenv("TW_ACCESS_TOKEN_SECRET")
+        )
 
-resp = client.create_tweet(text="Test tweet from Tweepy v2!")
-print(resp.data["id"])
-print(resp.data["text"])
+    def post_tweet(self, text: str):
+        resp = self.client.create_tweet(text=text)
+        return resp.data["id"], resp.data["text"]
 
+# Usage
+if __name__ == "__main__":
+    twitter = TwitterClient()
+    tweet_id, tweet_text = twitter.post_tweet("Test tweet from Tweepy v2!")
+    print(tweet_id)
+    print(tweet_text)
