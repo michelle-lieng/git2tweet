ff37b357cd20ea5d89175d9e09a3a4487a1829ac
Michelle <michellelieng2020@gmail.com>
2025-08-12 23:40:37 +1000

    changed check_state to a class + edited functions
    changed the extract latest state info to output latest day number as well

--- check_state.py
+++ check_state.py
@@ -2,56 +2,62 @@ import os
 import json
 from datetime import datetime
 
-STATE_PATH = "state.json"
-INITIAL_STATE = {
-    "history": [],
-    "latest": {
-        "last_overall_post_date": None,
-        "current_day_number": 0,
-        "latest_posted_sha": None,
-        #"per_repo_last_posted_sha": {}, # TODO: MULTIPLE REPO LATER!
-        "last_tweet_id": None
+class TweetState:
+    STATE_PATH = "state.json"
+    INITIAL_STATE = {
+        "history": [],
+        "latest": {
+            "last_overall_post_date": None,
+            "current_day_number": 0,
+            "latest_posted_sha": None,
+            #"per_repo_last_posted_sha": {}, # TODO: MULTIPLE REPO LATER!
+            "last_tweet_id": None
+        }
     }
-}
-
-def extract_latest_sha(path=STATE_PATH):
-    if not os.path.exists(path):
-        with open(path, "w", encoding="utf-8") as f:
-            json.dump(INITIAL_STATE, f, indent=2)
-        print(f"Created new state file at {path}")
-        return None
-    else:
-        print(f"State file already exists at {path}")
+    def extract_latest_state_info(self, path=STATE_PATH):
+        """
+        Extracts latest sha + day number
+        """
+        if not os.path.exists(path):
+            with open(path, "w", encoding="utf-8") as f:
+                json.dump(self.INITIAL_STATE, f, indent=2)
+            print(f"Created new state file at {path}")
+            return None,1
+        else:
+            print(f"State file already exists at {path}")
+            with open(path, "r", encoding="utf-8") as f:
+                state = json.load(f)
+            latest_sha = state.get("latest").get("latest_posted_sha")
+            latest_day_number = state.get("latest").get("current_day_number")
+            print(f"Latest posted SHA: {latest_sha}")
+            return latest_sha, latest_day_number
+    
+    def update_state(self, tweet_id, repo_name, repo_commits, path=STATE_PATH):
+        date_today = datetime.now().strftime("%Y-%m-%d")
         with open(path, "r", encoding="utf-8") as f:
             state = json.load(f)
-        latest_sha = state.get("latest").get("latest_posted_sha")
-        print(f"Latest posted SHA: {latest_sha}")
-        return latest_sha
-    
-def update_state(tweet_id, repo_name, repo_commits, path=STATE_PATH):
-    date_today = datetime.now().strftime("%Y-%m-%d")
-    with open(path, "r", encoding="utf-8") as f:
-        state = json.load(f)
-    history = state.get("history")
-    day_number = len(history) + 1
-    history.append({"date_today": date_today,
-                    "day_number": day_number,
-                    "tweet_id": tweet_id,
-                    "repo": {
-                        repo_name: repo_commits
-                    }
-                    })
-    latest = state.get("latest")
-    latest["last_overall_post_date"] = date_today
-    latest["current_day_number"] = day_number
-    latest["latest_posted_sha"] = repo_commits[-1]["short_sha"]
-    latest["last_tweet_id"]=  tweet_id
-    with open(path, "w", encoding="utf-8") as f:
-        json.dump(state, f, indent=2)
-    print("UPDATED!")
+        history = state.get("history")
+        day_number = len(history) + 1
+        history.append({"date_today": date_today,
+                        "day_number": day_number,
+                        "tweet_id": tweet_id,
+                        "repo": {
+                            repo_name: repo_commits
+                        }
+                        })
+        latest = state.get("latest")
+        latest["last_overall_post_date"] = date_today
+        latest["current_day_number"] = day_number
+        latest["latest_posted_sha"] = repo_commits[-1]["short_sha"]
+        latest["last_tweet_id"]=  tweet_id
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(state, f, indent=2)
+        print("UPDATED!")
 
 if __name__ == "__main__":
-    extract_latest_sha()
+    check_state = TweetState()
+    check_state.extract_latest_sha()
+    
     repo_commits = [{'body': 'simple initial layout + set up basic code to test the twitter api',
   'short_sha': '321368c',
   'subject': 'initial commit'},
@@ -70,4 +76,4 @@ if __name__ == "__main__":
  {'short_sha': 'c1819f0', 'subject': 'reformat to a class'},
  {'short_sha': '3616107', 'subject': 'reformat to a class'}]
     repo_name = "git2tweet"
-    update_state("1234",repo_name, repo_commits)
+    check_state.update_state("1234",repo_name, repo_commits)
